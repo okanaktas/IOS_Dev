375ba4e13c3bd82a1f8d33ce9c7c7903a7d60ebd
Okan Aktaş <okanaktas02@gmail.com>
2025-06-04 20:41:25 +0300

    feature(added connections)

--- Catch the Mole/Catch the Mole/GameViewController.swift
+++ Catch the Mole/Catch the Mole/GameViewController.swift
@@ -8,7 +8,22 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    var counter = 30
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var imageView0: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    @IBOutlet weak var imageView6: UIImageView!
+    @IBOutlet weak var imageView7: UIImageView!
+    @IBOutlet weak var imageView8: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Catch the Mole/Catch the Mole/ViewController.swift
+++ Catch the Mole/Catch the Mole/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func playButton(_ sender: Any) {
+        
+        performSegue(withIdentifier: "playGame", sender: self)
+        
+    }
+    
 }
 
