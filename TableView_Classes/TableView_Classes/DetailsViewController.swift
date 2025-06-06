21c8e45cd15b98a583c13f2fd4fe65ce6541687d
Okan Aktaş <okanaktas02@gmail.com>
2025-06-06 04:11:56 +0300

    Created TableView_Classes

---
+++ TableView_Classes/TableView_Classes/DetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.swift
+//  TableView_Classes
+//
+//  Created by Okan Aktas on 6.06.2025.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TableView_Classes/TableView_Classes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView_Classes
+//
+//  Created by Okan Aktas on 6.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
