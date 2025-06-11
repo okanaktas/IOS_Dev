f449fb044e86a5b225c4080a983c76d532a3bbe8
Okan Aktaş <okanaktas02@gmail.com>
2025-06-11 21:32:43 +0300

    Created ArtBook

---
+++ Recap/Recap/DetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailsViewController.swift
+//  Recap
+//
+//  Created by Okan Aktas on 11.06.2025.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+    
+    
+}
---
+++ Recap/Recap/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Recap
+//
+//  Created by Okan Aktas on 11.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addImage))
+        
+    }
+    
+    @objc func addImage(){
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+
+
+}
+
