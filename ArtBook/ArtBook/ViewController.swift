800a97e9ad195c1be2cea90b74e3c06c8906154c
Okan Aktaş <okanaktas02@gmail.com>
2025-06-11 23:10:51 +0300

    feature(added tableView Delegate and dataSource)

--- ArtBook/ArtBook/ViewController.swift
+++ ArtBook/ArtBook/ViewController.swift
@@ -7,14 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource{
+    
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addImageButton))
         
@@ -22,6 +24,21 @@ class ViewController: UIViewController {
     }
     
     @objc func addImageButton() {
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var context = cell.defaultContentConfiguration( )
+        context.text = "Hello, World!"
+        cell.contentConfiguration = context
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 
