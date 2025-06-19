db2c6720578dc6e154e4b5eb79786bd714886062
Okan Aktaş <okanaktas02@gmail.com>
2025-06-19 17:44:45 +0300

    update

--- IBAN Storage/IBAN Storage/DetailsViewController.swift
+++ IBAN Storage/IBAN Storage/DetailsViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var bankName: UITextField!
@@ -30,6 +31,29 @@ class DetailsViewController: UIViewController {
     }
     
     @IBAction func saveButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newValues = NSEntityDescription.insertNewObject(forEntityName: "Values", into: context)
+        
+        newValues.setValue(bankName.text, forKey: "bank")
+        
+        if let iban = Int(ibanNo.text!){
+            newValues.setValue(iban, forKey: "iban")
+        }
+        
+        newValues.setValue(UUID(), forKey: "id")
+        
+        let imageView = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        newValues.setValue(imageView, forKey: "image")
+        
+        do{
+            try context.save()
+            print("Success")
+        }catch{
+            print("Error")
+        }
         
         
     }
@@ -40,7 +64,15 @@ class DetailsViewController: UIViewController {
     
     
     @objc func tapImage(){
-        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage
+        self.dismiss(animated: true)
+    }
 }
--- IBAN Storage/IBAN Storage/ViewController.swift
+++ IBAN Storage/IBAN Storage/ViewController.swift
@@ -6,36 +6,93 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-   
-  
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var idArray: [UUID] = []
+    var bankArray: [String] = []
+    var ibanArray: [String] = []
+    var imageArray: [UIImage] = []
+
     @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.dataSource = self
         tableView.delegate = self
-        
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
-    
-        
+
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addTapped)
+        )
+
+        getData()
     }
-    
-    
+
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Values")
+        fetchRequest.returnsObjectsAsFaults = false
+
+        do {
+            let results = try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject] {
+
+                if let imageData = result.value(forKey: "image") as? Data,
+                   let image = UIImage(data: imageData) {
+                    imageArray.append(image)
+                }
+
+                if let id = result.value(forKey: "id") as? UUID {
+                    idArray.append(id)
+                }
+
+                if let bank = result.value(forKey: "bank") as? String {
+                    bankArray.append(bank)
+                }
+
+                if let iban = result.value(forKey: "iban") as? String {
+                    ibanArray.append(iban)
+                }
+            }
+
+            tableView.reloadData()
+
+        } catch {
+            print("Veri çekme hatası: \(error.localizedDescription)")
+        }
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return min(bankArray.count, ibanArray.count, imageArray.count)
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        var content = cell.defaultContentConfiguration()
+
+        if indexPath.row < bankArray.count {
+            content.text = bankArray[indexPath.row]
+        }
+
+        if indexPath.row < ibanArray.count {
+            content.secondaryText = ibanArray[indexPath.row]
+        }
+
+        if indexPath.row < imageArray.count {
+            content.image = imageArray[indexPath.row]
+        }
+
+        cell.contentConfiguration = content
         return cell
     }
-    
-    @objc func addTapped(){
+
+    @objc func addTapped() {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
-
-
 }
-
