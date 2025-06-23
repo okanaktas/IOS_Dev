9173c30fb8c252173970c471afc34f4483965a82
Okan Aktaş <okanaktas02@gmail.com>
2025-06-23 19:37:09 +0300

    feature(added navigaitonBarItem)

---
+++ CoreData - Art Book/CoreData - Art Book/DetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailsViewController.swift
+//  CoreData - Art Book
+//
+//  Created by Bugra Okan Aktas on 23.06.2025.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        print("Success")
+        
+    }
+    
+}
--- CoreData - Art Book/CoreData - Art Book/ViewController.swift
+++ CoreData - Art Book/CoreData - Art Book/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButton))
+        
+        
+    }
+    
+    @objc func addButton(){
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 
 
