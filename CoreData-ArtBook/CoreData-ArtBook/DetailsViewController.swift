f8b3761e60991958d927fafcca18190678a69758
Okan Aktaş <okanaktas02@gmail.com>
2025-06-23 22:30:32 +0300

    feature(added gestureRecognizer)

--- CoreData-ArtBook/CoreData-ArtBook/DetailsViewController.swift
+++ CoreData-ArtBook/CoreData-ArtBook/DetailsViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
-
+class DetailsViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var artistText: UITextField!
@@ -17,9 +17,14 @@ class DetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        //Recognizer
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
         
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
         
         
     }
@@ -31,5 +36,23 @@ class DetailsViewController: UIViewController {
     @objc func hideKeyboard(){
         view.endEditing(true)
     }
+    
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        //Kullanıcı seçtiği görseli editleyebilmesi için
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    //Medya seçildikten sonra ne olacağı, seçilen görsel any dönüyor
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage
+        //açtığım picker'ı kapatmak için
+        self.dismiss(animated: true)
+    }
+    
+    
     
 }
